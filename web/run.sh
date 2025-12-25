f343f7a4a893aafecb2483f3b6afdda496076ecc
Alexander Andersson <alexander.hjelm@solidify.dev>
2025-12-25 11:36:38 +0100

    Remove run.sh, update.sh

--- web/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export FLASK_APP="app"
-export FLASK_ENV="development"
-
-python3 init_db.py
-flask run --host=0.0.0.0
--- web/update.sh
+++
@@ -1,3 +0,0 @@
-systemctl stop food-app
-git pull
-systemctl start food-app
